cddf4c2f7312e18609d8ec93a5a507798f38c488
mario5609 <150905367+mario5609@users.noreply.github.com>
2024-04-04 11:24:08 +0530

    second commit

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,31 @@
+import { forwardRef, useImperativeHandle, useRef } from "react"
 import "./Input.css"
 
-export default function Input({className, type, placeholder, onChange, id}){
-    return(
-        <input className={className} type={type} placeholder={placeholder} onChange={onChange} id={id}/>
+ export default function Input({className, type, placeholder, onChange, id}){
+     return(
+         <input className={className} type={type} placeholder={placeholder} onChange={onChange} id={id}/>
+     );
+ }
+/*-----------------------------trying imperative handle and useRef----------------------------------------------------------------------*/
+/*
+const Input = forwardRef(({className, type, placeholder, onChange, id}, ref) => {
+    const inputRef = useRef(null);
+    useImperativeHandle(ref, () => ({
+        focusInput:() =>{
+            inputRef.current.focus()
+        },
+        assignText:() => {
+            inputRef.current.value = "Hello World"
+        },
+        TextColor:() =>{
+            inputRef.current.style.color = "orange"
+        }
+    }))
+        return(
+        <input ref={inputRef} className={className} type={type} placeholder={placeholder} onChange={onChange} id={id}/>
     );
-}
+})
+
+export default Input
+
+*/
--- src/views/Home/SearchItems.jsx
+++ src/views/Home/SearchItems.jsx
@@ -1,9 +1,17 @@
 import Input from '../../components/Input/Input'
 import Button from '../../components/Button/Button'
+import { useEffect, useRef } from 'react'
 
 export default function SearchItems({ setValue }) {
 
+  const ref = useRef(null);
+
   function handleOnClick() {
+    /*
+     ref.current.focusInput()
+     ref.current.assignText()
+     ref.current.TextColor()
+     */
     setValue(search.value)
   }
 
@@ -15,9 +23,17 @@ export default function SearchItems({ setValue }) {
     setValue("");
   }
 
+  /*
+   useEffect(() => {
+     fetch('https://jsonplaceholder.typicode.com/todos').then((res) => console.log(res))
+   },[])
+
+  // sessionStorage.setItem("test", "hello world")
+  */
+
   return (
     <div className='SearchItems'>
-      <Input placeholder={"Enter the word"} id={"search"} />
+      <Input ref={ref} placeholder={"Enter the word"} id={"search"} />
       <div className='SearchItemsButtons'>
         <Button className={"green"} type={"submit"} label={"Search"} onClick={handleOnClick} />
         <Button className={"blue"} type={"reset"} label={"Reset"} onClick={handleOnReset} />
